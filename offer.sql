9a8c88d1b8bdcc2b77d26ea84bc73972f0ae7591
rchagas <raphaelchagas_@hotmail.com>
2020-02-21 01:56:08 -0300

    Novos exemplos de Ofertas

--- offer.sql
+++ offer.sql
@@ -1,4 +1,6 @@
 INSERT INTO `SwCommerce`.`Offer` (`Id`, `Name`, `TypeOffer`, `MinSale`, `PackSize`, `Discount`) 
-VALUES ('1', 'NoOffer', '1', '1', '1', '0'),
-       ('2', 'Lev2Pag1', '2', '2', '2', '1'),
-       ('3', 'Lev3Por10', '3', '3', '3', '10');
+VALUES ('1', 'Sem Oferta', '1', '1', '1', '0'),
+       ('2', 'Leve 2 Pague 1', '2', '2', '2', '1'),
+       ('3', 'Leve 3 por 10', '3', '3', '3', '10'),
+       ('4', '30% de Desconto', '2', '1', '1', '0.3'),
+       ('2', 'Cupom 10 reais', '1', '1', '1', '10');
